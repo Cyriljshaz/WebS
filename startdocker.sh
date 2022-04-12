19f9187562b4c92a8e4024699b0ef9e044c131e1
Sebastien Lerda <sebastien.lerda3d@gmail.com>
2022-04-12 18:05:21 +0200

    wip

--- startdocker.sh
+++ startdocker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash  
-IMG_NAME="webscrapper"
+IMG_NAME="seblrd/webscrapper"
 PROC_NAME="proc_webscrapper"
 PATHTOPROJ="C:\Users\sebas\Documents\WORK\webscrapper"
 
 docker build -t $IMG_NAME .
 docker rm -f $IMG_NAME
-docker run --name $IMG_NAME -dp 3000:80 -v "$PATHTOPROJ:/usr/share/nginx/html" $IMG_NAME
-
-docker ps
+#docker run --name $IMG_NAME -dp 3000:80 -v "$PATHTOPROJ:/usr/share/nginx/html" $IMG_NAME
+docker-compose up -d
+#docker ps
 
