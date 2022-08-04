4028b22c332ba32a1204d15cbafa0d24be8c9e71
Sebastien Lerda <sebastien.lerda3d@gmail.com>
2022-08-04 18:15:19 +0200

    Wip multi pic

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -1,34 +1,34 @@
 const { timeStamp } = require("console");
-const puppeteer = require("puppeteer");
+const fs = require('fs');
 
-async function TEST() {
-    console.log("Starting puppeteer...");
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-        ]
-    });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 800 });
-    await page.goto("https://google.com");
-    const ss = await page.screenshot({ path: "./screenshot.jpg" });
-    await page.click("#W0wltc");
-    const ss1 = await page.screenshot({ path: "./screenshotCLick.jpg" });
-    var a = await page.$eval(".lnXdpd", element => element.getAttribute("src"));
+// async function TEST() {
+//     console.log("Starting puppeteer...");
+//     const browser = await puppeteer.launch({
+//         headless: true,
+//         args: [
+//             "--disable-setuid-sandbox",
+//             "--no-sandbox",
+//         ]
+//     });
+//     const page = await browser.newPage();
+//     await page.setViewport({ width: 1280, height: 800 });
+//     await page.goto("https://google.com");
+//     const ss = await page.screenshot({ path: "./screenshot.jpg" });
+//     await page.click("#W0wltc");
+//     const ss1 = await page.screenshot({ path: "./screenshotCLick.jpg" });
+//     var a = await page.$eval(".lnXdpd", element => element.getAttribute("src"));
 
 
-    const svgImage = await page.$('.lnXdpd');
-    await svgImage.screenshot({
-        path: 'test2.png',
-        omitBackground: true,
-    });
-    console.log(a);
-    await page.close();
-    await browser.close();
+//     const svgImage = await page.$('.lnXdpd');
+//     await svgImage.screenshot({
+//         path: 'test2.png',
+//         omitBackground: true,
+//     });
+//     console.log(a);
+//     await page.close();
+//     await browser.close();
 
-}
+// }
 
 
 async function saveImage(page) {
@@ -50,17 +50,19 @@ class ScrapWebsite {
 
     async getImagesFromUrl() {
         try {
-            await this.page.waitForNavigation()
-            const p = this.page;
-            const images = await p.$(this.selector);
-            var a = await this.page.$eval(images, element => element.getAttribute("src"));
-            console.log(a)
-            var counter = 1;
-            // images.forEach(async image => {
-            //     // await saveImage(image, counter);
-            //     // counter++;
-            // });
+            let imagesHref = await this.page.evaluate((sel) => {
+                listUrl = [];
+                document.querySelectorAll(sel).forEach(element => {
+                    listUrl.push(element.getAttribute('src').replace('/', ''))
+                });
+                return listUrl;
+            }, this.selector);
 
+            var counter = 1;
+            await imagesHref.forEach(async imageHref => {
+                await this.saveImage(imageHref);
+                counter++;
+            });
         } catch (error) {
             console.log("Can't get images from url : " + error);
         }
@@ -81,11 +83,15 @@ class ScrapWebsite {
         await this.page.setViewport({ width: 1280, height: 800 });
     }
 
-    async saveImage(img, counter) {
-        // const img = await page.$(this.selector);
-        await img.screenshot({
-            path: './img/test' + counter + '.png',
-            omitBackground: true,
+    async saveImage(imageHref, counter) {
+        var domain = `https://${this.getDomainUrl()}/`;
+        var viewSource = await this.page.goto(domain + imageHref);
+        fs.writeFile(`img/test/TEST-FILE-${counter}.png`, await viewSource.buffer(), function (err) {
+            if (err) {
+                return console.log("Error in writting file :: " + err);
+            }
+
+            console.log("The file was saved!");
         });
     }
 
@@ -94,23 +100,35 @@ class ScrapWebsite {
         await this.page.goto(url);
     }
 
+    getDomainUrl() {
+        const url = (new URL(this.url)).hostname;
+        console.log(url);
+        return url;
+    }
+
     async closeBrowser() {
         await this.page.close();
         await this.browser.close();
+    }
+
+    async runJob() {
+        try {
+            await this.buildBrowser()
+            await this.goToUrl(this.url);
+            await this.getImagesFromUrl();
+            console.log("Done");
+        } catch (error) {
+            console.log("Can't run job : " + error);
+        } finally {
+            console.log("Closing browser...");
+            await scrap.closeBrowser();
+        }
     }
 }
 
 
 const scrap = new ScrapWebsite();
-scrap.buildBrowser()
-    .then(() => {
-        scrap.goToUrl("https://www.tutorialspoint.com/puppeteer/puppeteer_id_selector.htm");
-        scrap.selector = "#mainContent img";
-    })
-    .then(() => {
-        scrap.getImagesFromUrl();
-    })
-    .then(() => {
-        console.log("Closing browser...");
-        // scrap.closeBrowser();
-    });
+scrap.domain = "https://www.tutorialspoint.com/puppeteer/puppeteer_id_selector.htm";
+scrap.url = "https://www.tutorialspoint.com/puppeteer/puppeteer_id_selector.htm";
+scrap.selector = "img";
+scrap.runJob();
