23c2753f359d91ba508ab7633f3631d45dc4fd37
Sebastien Lerda <sebastien.lerda3d@gmail.com>
2022-08-04 19:41:12 +0200

    clean code

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -1,45 +1,6 @@
 const { timeStamp } = require("console");
 const fs = require('fs');
 
-// async function TEST() {
-//     console.log("Starting puppeteer...");
-//     const browser = await puppeteer.launch({
-//         headless: true,
-//         args: [
-//             "--disable-setuid-sandbox",
-//             "--no-sandbox",
-//         ]
-//     });
-//     const page = await browser.newPage();
-//     await page.setViewport({ width: 1280, height: 800 });
-//     await page.goto("https://google.com");
-//     const ss = await page.screenshot({ path: "./screenshot.jpg" });
-//     await page.click("#W0wltc");
-//     const ss1 = await page.screenshot({ path: "./screenshotCLick.jpg" });
-//     var a = await page.$eval(".lnXdpd", element => element.getAttribute("src"));
-
-
-//     const svgImage = await page.$('.lnXdpd');
-//     await svgImage.screenshot({
-//         path: 'test2.png',
-//         omitBackground: true,
-//     });
-//     console.log(a);
-//     await page.close();
-//     await browser.close();
-
-// }
-
-
-async function saveImage(page) {
-    const svgImage = await page.$('.lnXdpd');
-    await svgImage.screenshot({
-        path: 'test2.png',
-        omitBackground: true,
-    });
-}
-
-
 class ScrapWebsite {
 
     constructor() {
